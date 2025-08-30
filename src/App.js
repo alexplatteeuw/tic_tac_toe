2841ca22e0ef5906f19819fe8d10c706e4c299d9
Alex Platteeuw <alexplatteeuw@gmail.com>
2025-08-30 15:37:30 +0200

    declaring a winner

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true)
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   // JavaScript supports closures which means an inner function (e.g. handleClick) has access
   // to variables and functions defined in an outer function (e.g. Board). The handleClick function 
   // can read the squares state and call the setSquares method because they are both defined inside of
   // the Board function.
   function handleClick(i) {
-    if (squares[i]) { return }
-    
+    if (squares[i] || calculateWinner(squares) ) { return }
+
     // We make a copy of the current state (squares) instead of changing it directly.
     // This is important because React state is immutable: mutating it directly
     // (e.g. squares[i] = "X") would keep the same array reference, and React might
@@ -25,6 +33,7 @@ export default function Board() {
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         < Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         < Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -46,4 +55,26 @@ export default function Board() {
 
 function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    // check if there is a non-empty symbol (X or O) in square a, and is that same symbol also in squares b and c
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
